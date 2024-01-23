d26f0c435be5251fe931d6700ff5f226caf576d6
Mike Kurkin <mike@kurkin.net>
2024-01-24 01:18:41 +0300

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    const folder = 'files';
+    const filename = 'fresh.txt';
+    const content = 'I am fresh and young';
+
+    const filePath = join(import.meta.dirname, folder, filename);
+    try {
+        await writeFile(filePath, content, { flag: 'wx+' });
+    } catch(err) {
+        if (err.code == 'EEXIST') {
+            throw(new Error('FS operation failed'));
+        } else console.error(err);
+    }
 };
 
 await create();
