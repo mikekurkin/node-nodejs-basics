a1d1d608f01cb4adaaeb312cbdfd10ae9a4d0047
Mike Kurkin <mike@kurkin.net>
2024-01-24 21:49:36 +0300

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { FSError } from './helpers.js';
+
 const read = async () => {
-    // Write your code here 
+    const folder = 'files';
+    const filename = 'fileToRead.txt'
+
+    const filePath = join(import.meta.dirname, folder, filename);
+    try {
+        const content = await readFile(filePath, { encoding: 'utf8' });
+        console.log(content);
+    } catch(err) {
+        if (err.code == 'ENOENT') {
+            throw new FSError();
+        } else console.error(err);
+    }
 };
 
 await read();
