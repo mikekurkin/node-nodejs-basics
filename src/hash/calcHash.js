bdad84e90c9dc7240ac26f2cf7528fb2b4937fac
Mike Kurkin <mike@kurkin.net>
2024-01-26 01:03:46 +0300

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { open } from 'node:fs/promises';
+import { join } from 'node:path';
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filename = 'files/fileToCalculateHashFor.txt';
+
+    const hash = createHash('sha256');
+
+    const filePath = join(import.meta.dirname, filename);
+    const file = await open(filePath, 'r');
+    const stream = file.createReadStream();
+
+    stream.pipe(hash).setEncoding('hex').pipe(process.stdout);
 };
 
 await calculateHash();
