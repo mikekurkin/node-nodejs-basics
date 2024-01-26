693cd83ccd4b8486f172fde1ea063d5a61f3ef52
Mike Kurkin <mike@kurkin.net>
2024-01-26 20:47:42 +0300

    feat: implement read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { open } from 'node:fs/promises';
+import { join } from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+    const filename = 'files/fileToRead.txt';
+
+    const filePath = join(import.meta.dirname, filename);
+    const file = await open(filePath, 'r');
+    const stream = file.createReadStream();
+
+    stream.pipe(process.stdout);
 };
 
 await read();
